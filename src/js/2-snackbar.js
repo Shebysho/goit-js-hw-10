5b817f7fdd806ec31682a540bb525ec6cc6c66e8
Aargo911 <jekaboy2019@gmail.com>
2024-10-25 17:25:59 +0300

    last-commit

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,43 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const { delay, state } = event.currentTarget.elements;
+
+  createPromise(state.value, Number(delay.value))
+    .then(({ delay }) => {
+      iziToast.success({
+        title: false,
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+        displayMode: 2,
+      });
+    })
+    .catch(({ delay }) => {
+      iziToast.error({
+        title: false,
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+        displayMode: 2,
+      });
+    });
+});
